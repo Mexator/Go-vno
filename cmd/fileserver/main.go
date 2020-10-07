9d9847bdb64140061809ea8180b840899b1a6d2a
Anton Brisilin <a.brisilin@innopolis.university>
2020-10-07 15:37:53 +0300

    working on auto detect of fileservers

--- cmd/fileserver/main.go
+++ cmd/fileserver/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"log"
@@ -9,6 +10,8 @@ import (
 	"github.com/Mexator/Go-vno/pkg/fileserver"
 
 	api "github.com/Mexator/Go-vno/pkg/api/fileserver"
+	nsapi "github.com/Mexator/Go-vno/pkg/api/nameserver"
+
 	"google.golang.org/grpc"
 )
 
@@ -20,13 +23,17 @@ var (
 		"/var/data",
 		"Folder to stored files on the server",
 	)
+	serverURL = flag.String(
+		"s",
+		"nameserver:3000",
+		"hostname of the nameserver")
 )
 
 func main() {
 	flag.Parse()
 
 	s := grpc.NewServer()
-	srv, err := fileserver.MakeFileServer(flag.Arg(0))
+	srv, err := fileserver.MakeFileServer(*storageDir)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -37,7 +44,18 @@ func main() {
 		log.Fatal(err)
 	}
 
+	go attachToNS(*serverURL)
 	if err := s.Serve(listener); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func attachToNS(serverAddress string) {
+	var grpcopts = []grpc.DialOption{grpc.WithInsecure()}
+	conn, err := grpc.Dial(serverAddress, grpcopts...)
+	if err != nil {
+		log.Fatal("Can not connect to name server")
+	}
+	client := nsapi.NewNameServerClient(conn)
+	client.ConnectFileServer(context.Background(), &nsapi.ConnectRequest{})
 }
